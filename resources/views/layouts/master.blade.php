7e42d460bc65cc89c8b9c082c183870e97e4abb5
beniwein <beni_weinmann@hotmail.com>
2022-06-01 16:44:44 +0200

    scss-small-changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-  <link href="/css/app.css" rel="stylesheet">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
    <!-- hier wird der Wert von der section "title" eines blade templates ausgefüllt,
    welches dieses layout "extended" -->
    <title class="title">@yield('title')</title>
