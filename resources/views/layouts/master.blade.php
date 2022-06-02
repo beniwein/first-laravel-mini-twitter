3402bad4cddb1a8b0f40984865791b240f0a5725
beniwein <beni_weinmann@hotmail.com>
2022-06-02 14:03:22 +0200

    small-correction

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,10 +5,10 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
    <!-- hier wird der Wert von der section "title" eines blade templates ausgefüllt,
    welches dieses layout "extended" -->
-   <img src="{{ asset('images/logo.png') }}" alt="Pacman">
+   <img src="{{ asset('/images/logo.png') }}" alt="Pacman">
    <title class="title">@yield('title')</title>
 </head>
 <body>
